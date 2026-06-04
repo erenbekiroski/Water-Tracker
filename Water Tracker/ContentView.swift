91ce98280bc276d2172575a487584272a5e05cbd
Eren Bekiroski <eren.bekiroski@web.de>
2026-06-04 18:58:14 +0200

    Initial Commit

---
+++ Water Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Water Tracker
+//
+//  Created by Eren on 04.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Water Tracker/Water_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Water_TrackerApp.swift
+//  Water Tracker
+//
+//  Created by Eren on 04.06.26.
+//
+
+import SwiftUI
+
+@main
+struct Water_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
